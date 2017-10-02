370dee3755b987cae75212b45f3fad700d13719b
MelanieOC <melanie.ocharan@gmail.com>
2017-10-02 07:16:54 -0500

    arreglando error doble click

--- app.jsx
+++ app.jsx
@@ -1,4 +1,4 @@
-let preguntas = [
+const preguntas = [
   {
     pregunta: 'Which is the oldest airline in the world?',
     opciones: { A: 'Avianca', B: 'KLM', C: 'Qantas' },
@@ -52,7 +52,8 @@ const RedesSociales = () => {
 class Application extends React.Component {
   constructor(props) {
     super(props);
-
+    this.preguntas=props.preguntas;
+    this.marcar=true;
     this.state = {
       contar: 0,
       respuestas: [],
@@ -62,7 +63,7 @@ class Application extends React.Component {
     }
   }
   siguiente() {
-    if (this.state.contar === preguntas.length - 1) {
+    if (this.state.contar === this.preguntas.length - 1) {
       this.setState({
         completo: true
       });
@@ -73,7 +74,7 @@ class Application extends React.Component {
 
   }
   anterior(){
-    if (this.state.contar === preguntas.length) {
+    if (this.state.contar === this.preguntas.length) {
       this.setState({
         completo: false
       });
@@ -83,20 +84,23 @@ class Application extends React.Component {
     })
   }
   guardarRespuesta(evento, value) {
+   if(this.marcar){
+     this.marcar=false;
     let res = this.state.respuestas;
     res[this.state.contar] = value;
     this.setState({
       respuestas: res
     })
-    if (value == preguntas[this.state.contar].respuesta) {
+    if (value == this.preguntas[this.state.contar].respuesta) {
       this.setState({
         correctas: this.state.correctas + 1
       })
     }
     let t = setTimeout(()=>{
+      this.marcar=true;
       this.siguiente();
-    }, 900);
-    
+    }, 700);
+   }
   }
   opciones(opciones) {
     return Object.keys(opciones).map((key, index) => {
@@ -106,12 +110,12 @@ class Application extends React.Component {
       </div>);
     })
   }
-  preguntas() {
+  crearPreguntas() {
     return (
       <div>
-        <h1 className="text-center"> {preguntas[this.state.contar].pregunta} </h1>
+        <h1 className="text-center"> {this.preguntas[this.state.contar].pregunta} </h1>
         <div className="opciones row">
-          {this.opciones(preguntas[this.state.contar].opciones)}
+          {this.opciones(this.preguntas[this.state.contar].opciones)}
         </div>
       </div>
     );
@@ -121,15 +125,17 @@ class Application extends React.Component {
       <div id='respuestas'>
         <h1 className="text-center">
           {!this.state.comparar && 'Here are you answers:'}
-          {this.state.comparar && this.state.correctas + ' out of ' + preguntas.length + ' correct!'}
+          {this.state.comparar && this.state.correctas===0 && 'Ooops, ' + this.state.correctas + ' out of ' + this.preguntas.length + ' correct!'}
+          {this.state.comparar && this.state.correctas===this.preguntas.length && 'Wow, ' + this.state.correctas + ' out of ' + this.preguntas.length + ' correct!'}
+          {this.state.comparar &&  this.state.correctas + ' out of ' + this.preguntas.length + ' correct!'}
         </h1>
         {this.state.respuestas.map((a, i) => {
-          if (a == preguntas[i].respuesta && this.state.comparar) {
-            return <p className="text-success">{i + 1}. {preguntas[i].pregunta}<strong>{a}</strong></p>
+          if (a == this.preguntas[i].respuesta && this.state.comparar) {
+            return <p className="text-success">{i + 1}. {this.preguntas[i].pregunta}<strong>{a}</strong></p>
           } else if (this.state.comparar) {
-            return <p className="text-danger">{i + 1}. {preguntas[i].pregunta}<strong><strike>{a}</strike> {preguntas[i].respuesta}</strong></p>
+            return <p className="text-danger">{i + 1}. {this.preguntas[i].pregunta}<strong><strike>{a}</strike> {this.preguntas[i].respuesta}</strong></p>
           } else {
-            return <p>{i + 1}. {preguntas[i].pregunta}<strong>{a}</strong></p>;
+            return <p>{i + 1}. {this.preguntas[i].pregunta}<strong>{a}</strong></p>;
           }
         })
         }
@@ -142,13 +148,7 @@ class Application extends React.Component {
     );
 
   }
-  /* let expresion='';
-   if(this.state.correctas==0){
-     expresion='Ooops, ';
-   } else if(this.state.correctas==preguntas.length) {
-     expresion='Wow, ';
-   } 
-   <h1 class="text-center">{expresion}{this.state.correctas} out of {preguntas.length} correct!</h1>*/
+  
   comparar() {
     this.setState({
       comparar: true
@@ -167,14 +167,14 @@ class Application extends React.Component {
     return (
       <div className="container">
         <header className="text-center">
-          {!this.state.completo && <img src={preguntas[this.state.contar].imagen} />}
+          {!this.state.completo && <img src={this.preguntas[this.state.contar].imagen} />}
           {this.state.completo && <img src="assets/img/truck.svg" />}
         </header>
         <div className="content">
           {!this.state.comparar &&
             <div id="progreso">
               <div className="progress-label">
-                {this.state.respuestas.length} of {preguntas.length} answered
+                {this.state.respuestas.length} of {this.preguntas.length} answered
             </div>
               <div className="progress">
                 <div className="progress-bar" role="progressbar" aria-valuemax="100" style={{ width: this.state.respuestas.length * 20 + '%', height: '5px' }}>
@@ -183,17 +183,17 @@ class Application extends React.Component {
             </div>
           }
           <div id="prueba">
-            {!this.state.completo && this.preguntas()}
+            {!this.state.completo && this.crearPreguntas()}
             {this.state.completo && this.listarRespuestas()}
           </div>
           <RedesSociales />
         </div>
         {!this.state.comparar && this.state.respuestas.length != 0 &&
           <div id="flechas" className="text-center">
-            <button id="anterior" className={this.state.respuestas.length>=this.state.contar&&this.state.contar?'btn':"btn disabled"} onClick={()=>this.anterior()}>
+            <button id="anterior" className={this.state.respuestas.length>=this.state.contar&&this.marcar&&this.state.contar?'btn':"btn disabled"} onClick={()=>this.anterior()}>
               <img className="img-responsive" src="assets/img/navigation-left-arrow.svg" alt="" />
             </button>
-            <button id="siguiente" className={this.state.respuestas.length>this.state.contar?'btn':"btn disabled"} onClick={()=>this.siguiente()}>
+            <button id="siguiente" className={this.state.respuestas.length>this.state.contar&this.marcar?'btn':"btn disabled"} onClick={()=>this.siguiente()}>
               <img className="img-responsive" src="assets/img/navigation-right-arrow.svg" alt="" />
             </button>
           </div>
@@ -203,4 +203,4 @@ class Application extends React.Component {
 }
 
 
-ReactDOM.render(<Application />, document.getElementById('container'));
+ReactDOM.render(<Application preguntas={preguntas}/>, document.getElementById('container'));
