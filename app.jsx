5285af76356803447d1ca2d6a3d1ab7c8973866a
MelanieOC <melanie.ocharan@gmail.com>
2017-10-01 20:08:21 -0500

    funcionalida mostrar respuesta

--- app.jsx
+++ app.jsx
@@ -48,6 +48,24 @@ const RedesSociales = () => {
     </div>
   );
 }
+const lista=(props)=>{
+  return (
+    <div id='respuestas'>
+      <h1 class="text-center">props.title</h1>
+      {props.array.map((a, i) => {
+        if(a==preguntas[i].respuesta && props.comparar){
+          return <p className="text-success">{i + 1}. {preguntas[i].pregunta}<strong>{a}</strong></p>
+        } else if(props.comparar) {
+          return <p className="text-danger">{i + 1}. {preguntas[i].pregunta}<strong><strike>{a}</strike> {preguntas[i].respuesta}</strong></p>
+        }else{
+          return <p>{i + 1}. {preguntas[i].pregunta}<strong>{a}</strong></p>;
+        }
+      })
+      }
+      <button className='btn-lg btn-dark' onClick={()=>props.funcion()}>{props.boton}</button>
+    </div>
+  );
+}
 class Application extends React.Component {
   constructor(props) {
     super(props);
@@ -55,7 +73,8 @@ class Application extends React.Component {
     this.state = {
       contar: 0,
       respuestas: [],
-      completo: false
+      completo: false,
+      enviado:false
     }
   }
   siguiente() {
@@ -105,12 +124,36 @@ class Application extends React.Component {
           );
         })
         }
-        <button className='btn-lg btn-dark' onClick={this.comparar()}></button>
+        <button className='btn-lg btn-dark' onClick={()=>this.comparar()}>Submit</button>
       </div>
     );
 
   }
-
+ /* let expresion='';
+  if(this.state.correctas==0){
+    expresion='Ooops, ';
+  } else if(this.state.correctas==preguntas.length) {
+    expresion='Wow, ';
+  } 
+  <h1 class="text-center">{expresion}{this.state.correctas} out of {preguntas.length} correct!</h1>*/
+  comparar(){
+    this.setState({    
+      enviado: true
+    });
+    return (
+      <div id='respuestas'>
+        {this.state.respuestas.map((a, i) => {
+          if(a==preguntas[i].respuesta){
+            return <p className="text-success">{i + 1}. {preguntas[i].pregunta}<strong>{a}</strong></p>
+          } else {
+            return <p className="text-danger">{i + 1}. {preguntas[i].pregunta}<strong><strike>{a}</strike> {preguntas[i].respuesta}</strong></p>
+          }
+        })
+        }
+        <button className='btn-lg btn-dark' onClick={this.comparar}>Submit</button>
+      </div>
+    );
+  }
   render() {
     return (
       <div className="container">
@@ -129,8 +172,9 @@ class Application extends React.Component {
             </div>
           </div>
           <div id="prueba">
-            {!this.state.completo && this.preguntas()}
+            {!this.state.completo && !this.state.enviado && this.preguntas()}
             {this.state.completo && this.listarRespuestas()}
+            {this.state.enviado && this.comparar()}
           </div>
           <RedesSociales />
         </div>
